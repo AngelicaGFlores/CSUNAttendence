b6dfdc3e9b9be3dca308c4d7d6a5188ef6888b4c
AngelicaGFlores <33407052+AngelicaGFlores@users.noreply.github.com>
2020-12-10 00:22:15 -0800

    Added the UI for Home Page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 export class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello Angie World</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="Logo">
+          CSUN
+          <br />
+          California
+          <br />
+          State University
+          <br />
+          Northrige
+        </div>
+        <div className="Image">
+          <h1>Image goes here</h1>
+        </div>
+        <div className="Navigation-Buttons">
+          <Button>Faculty</Button> <br/>
+          <Button>Student</Button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
