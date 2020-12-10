1b3339f0e7d45156149b459052567f2f49445882
Aaron Alvarez <alvarez.a.aaron@gmail.com>
2020-12-10 00:38:32 -0800

    added FacultyProfile.js

---
+++ src/components/FacultyProfile.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class FacultyProfile extends Component {
+    render() {
+        return (
+            <div>
+                {
+                // -- Image Tag --
+                }
+                {
+                // -- Navigation Buttons 3 : Approvals - Generate QR Code - Check Attendace --
+                }
+                {
+                // -- Faculty Information : Name - Id --
+                }
+            </div>
+        )
+    }
+}
+
+export default FacultyProfile
