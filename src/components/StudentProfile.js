ef8a98482158fffda97a9435ff07924992acce5d
AngelicaGFlores <33407052+AngelicaGFlores@users.noreply.github.com>
2020-12-10 00:56:57 -0800

    updated the UI for StudentProfile.js

--- src/components/StudentProfile.js
+++ src/components/StudentProfile.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 export class StudentProfile extends Component {
-    render() {
-        return (
-            <div>
-                 {
-                // -- Image Tag --
-                }
+  render() {
+    return (
+      <div>
+        {
+          // -- Image Tag --
+        }
+        <image>Image goes here </image>
 
-                {
-                // -- Navigation Buttons 3 : Approvals/Request - Read QR Code - Check Attendence --
-                }
+        {
+          // -- Navigation Buttons 3 : Approvals/Request - Read QR Code - Check Attendence --
+        }
+        <Button>Approvals/Request</Button>
+        <br />
+        <Button>Read QR Code</Button>
+        <br />
+        <Button>Check Attendence</Button>
+        <br />
 
-                {
-                // -- Student Information : Name - Id --
-                }
-            </div>
-        )
-    }
+        {
+          // -- Student Information : Name - Id --
+        }
+        <h2>FULL Name</h2>
+        <h3>Student ID</h3>
+      </div>
+    );
+  }
 }
 
-export default StudentProfile
+export default StudentProfile;
