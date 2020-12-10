a874b23207f4dec7629a9be7a5543707487b17fa
Aaron Alvarez <alvarez.a.aaron@gmail.com>
2020-12-09 23:57:26 -0800

    git init

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Angie World</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage
