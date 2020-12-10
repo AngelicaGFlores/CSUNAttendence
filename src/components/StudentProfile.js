5d19f23061e759b715f3887f6473fa5585cb2864
Aaron Alvarez <alvarez.a.aaron@gmail.com>
2020-12-10 00:46:13 -0800

    added StudentProfile.js

---
+++ src/components/StudentProfile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class StudentProfile extends Component {
+    render() {
+        return (
+            <div>
+                 {
+                // -- Image Tag --
+                }
+
+                {
+                // -- Navigation Buttons 3 : Approvals/Request - Read QR Code - Check Attendence --
+                }
+
+                {
+                // -- Student Information : Name - Id --
+                }
+            </div>
+        )
+    }
+}
+
+export default StudentProfile
