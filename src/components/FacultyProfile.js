d692a89e313b40e8309e13706eeeada88a2c3ca9
AngelicaGFlores <33407052+AngelicaGFlores@users.noreply.github.com>
2020-12-10 00:47:05 -0800

    updated the UI for FacultyProfile.js

--- src/components/FacultyProfile.js
+++ src/components/FacultyProfile.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 export class FacultyProfile extends Component {
-    render() {
-        return (
-            <div>
-                {
-                // -- Image Tag --
-                }
-                {
-                // -- Navigation Buttons 3 : Approvals - Generate QR Code - Check Attendace --
-                }
-                {
-                // -- Faculty Information : Name - Id --
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {
+          // -- Image Tag --
+        }
+        <image>Image goes here</image>
+        {
+          // -- Navigation Buttons 3 : Approvals - Generate QR Code - Check Attendence --
+        }
+        <Button>Approvals</Button>
+        <br />
+        <Button>generate QR Code</Button>
+        <br />
+        <Button>Check Attendence</Button>
+        <br />
+
+        {
+          // -- Faculty Information : Name - Id --
+        }
+        <h2>FULL Name</h2>
+        <h3>Faculty ID</h3>
+      </div>
+    );
+  }
 }
 
-export default FacultyProfile
+export default FacultyProfile;
